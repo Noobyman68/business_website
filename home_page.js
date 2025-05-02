ea299d2196606b71887e20af4c9bcb9dcd6b9e0c
Caleb Oglesby <noobyman68@gmail.com>
2025-05-02 18:04:52 -0400

    Adding previous work

---
+++ home_page.js
@@ -0,0 +1,21 @@
+
+const button = document.getElementById("");
+
+
+class IG_link extends HTMLElement{
+  constructor(){
+    super()
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <a href = "https://www.youtube.com/" rel = "noopener" target = "_blank">
+        <img class = "IG_img" src = "instagram-logo.webp" alt = "IG_LINK">
+      </a>`;
+      this.attachShadow({mode: 'open'}).appendChild(template.content.cloneNode(true));
+
+      const styles = document.createElement("link");
+      styles.setAttribute("rel", "stylesheet");
+      styles.setAttribute("href", "styles/home_page.css");
+      this.shadowRoot.appendChild(styles);
+  }
+}
+customElements.define("ig-link", IG_link);
